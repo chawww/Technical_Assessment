293a22273a88f3828a33310fd4bd2387b22bd784
chawww <129158539+chawww@users.noreply.github.com>
2026-05-28 10:59:33 +0800

    Add files via upload

---
+++ CoffeeProject.Infrastructure/Repositories/CoffeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using CoffeeProject.Application.DTOs;
+using CoffeeProject.Application.Repositories;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeProject.Infrastructure.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        public Task<CoffeeResponseDto> GetBrewCoffee()
+        {
+            try
+            {
+                var response = new CoffeeResponseDto
+                {
+                    Message = "Your piping hot coffee is ready",
+                    Prepared = DateTime.UtcNow
+                };
+
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while brewing coffee.", ex);
+            }
+        }
+    }
+}
