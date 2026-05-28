2faae1708c072b2b76f9ac5ef91d7150aedc2626
chawww <129158539+chawww@users.noreply.github.com>
2026-05-28 13:58:06 +0800

    Add files via upload

---
+++ CoffeeProject.Test/CoffeeMethodsTests.cs
@@ -0,0 +1,45 @@
+﻿using CoffeeProject.API.Endpoints;
+using CoffeeProject.Application.DTOs;
+using CoffeeProject.Application.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using Xunit;
+
+namespace CoffeeProject.Test.API;
+
+public class CoffeeMethodsTests
+{
+    [Fact]
+    public async Task GetBrewCoffee_ShouldReturnOk_WhenRepositoryReturnsData()
+    {
+        var mockRepo = new Mock<ICoffeeRepository>();
+
+        mockRepo.Setup(x => x.GetBrewCoffee())
+            .ReturnsAsync(new CoffeeResponseDto
+            {
+                Message = "Your piping hot coffee is ready",
+                Prepared = DateTime.UtcNow
+            });
+
+        var methods = new CoffeeMethods(mockRepo.Object);
+
+        var result = await methods.GetBrewCoffee();
+
+        Assert.IsType<Ok<CoffeeResponseDto>>(result);
+    }
+
+    [Fact]
+    public async Task GetBrewCoffee_ShouldReturnNotFound_WhenRepositoryReturnsNull()
+    {
+        var mockRepo = new Mock<ICoffeeRepository>();
+
+        mockRepo.Setup(x => x.GetBrewCoffee())
+            .ReturnsAsync((CoffeeResponseDto?)null);
+
+        var methods = new CoffeeMethods(mockRepo.Object);
+
+        var result = await methods.GetBrewCoffee();
+
+        Assert.IsType<NotFound>(result);
+    }
+}
