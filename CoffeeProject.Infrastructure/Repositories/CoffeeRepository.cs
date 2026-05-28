cc9d103d50403a510fb2382633342efcbc2e3c01
chawww <129158539+chawww@users.noreply.github.com>
2026-05-28 12:28:09 +0800

    Add files via upload

--- CoffeeProject.Infrastructure/Repositories/CoffeeRepository.cs
+++ CoffeeProject.Infrastructure/Repositories/CoffeeRepository.cs
@@ -1,25 +1,49 @@
 ﻿using CoffeeProject.Application.DTOs;
 using CoffeeProject.Application.Repositories;
-using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
 
 namespace CoffeeProject.Infrastructure.Repositories
 {
     public class CoffeeRepository : ICoffeeRepository
     {
-        public Task<CoffeeResponseDto> GetBrewCoffee()
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public CoffeeRepository(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public async Task<CoffeeResponseDto> GetBrewCoffee(string zipCode, string countryCode)
         {
             try
             {
+                var apiKey = _configuration["OpenWeatherAPI:ApiKey"] ?? "";
+
+                var location = await _httpClient
+                    .GetFromJsonAsync<GeoResponse>(
+                       $"geo/1.0/zip?zip={zipCode},{countryCode}&limit=1&appid={apiKey}");
+
+                WeatherResponse? weather = null;
+                if (location != null)
+                {
+                    weather = await _httpClient
+                        .GetFromJsonAsync<WeatherResponse>(
+                            $"data/2.5/weather?lat={location.Lat}&lon={location.Lon}&units=metric&appid={apiKey}");
+                }
+
+                string message = weather?.Main.Temp > 30 
+                        ? "Your refreshing iced coffee is ready"
+                        : "Your piping hot coffee is ready";
+
                 var response = new CoffeeResponseDto
                 {
-                    Message = "Your piping hot coffee is ready",
+                    Message = message,
                     Prepared = DateTime.UtcNow
                 };
 
-                return Task.FromResult(response);
+                return response;
             }
             catch (Exception ex)
             {
